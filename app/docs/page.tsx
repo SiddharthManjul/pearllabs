b76b455a2f0e598fb3a4ba6cb1f756666c517f46
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-04-11 23:22:45 +0530

    Initial Commit - Hero Section

---
+++ app/docs/page.tsx
@@ -0,0 +1,5 @@
+export default function Docs() {
+    return (
+        <h1>Dammit not another document!</h1>
+    )
+}
---
+++ components/custom/hero-grid.tsx
@@ -0,0 +1,11 @@
+import { RetroGrid } from "../magicui/retro-grid";
+
+export default function HeroGrid() {
+  return (
+    <div className="">
+      <RetroGrid 
+        angle={0}
+      />
+    </div>
+  );
+}
